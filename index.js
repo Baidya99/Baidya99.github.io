0ed31bdf0844792de4d6fe8c103ee5d06e3d5865
Baidya99 <kundubaidya99@gmail.com>
2017-02-05 13:20:43 +0530

    Added changed

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var head = function(){
+    scrollTop = $(window).scrollTop();
+    if(scrollTop>0){
+      $('#H').addClass('shadow');
+      $('#H').addClass('whiteb');
+      $('#H').removeClass('wtext');
+      $('#icon').removeClass('big');
+      $('#icon').addClass('small'); 
+    }else{
+      $('#H').removeClass('shadow');
+      $('#H').removeClass('whiteb');
+      $('#H').addClass('wtext');
+      $('#icon').removeClass('small');
+      $('#icon').addClass('big');
+    }
+  };
+  $(window).scroll(function() {
+	  head();
+  });
+});
